828307202e86f4985bc26c287238c1fe6b9c6318
Dan Parker <danparker55@gmail.comgit config --global user.name Dan>
2018-07-23 16:22:37 -0700

    nav bar complete

--- src/components/app.js
+++ src/components/app.js
@@ -3,10 +3,12 @@ import '../assets/css/app.css';
 import 'materialize-css/dist/css/materialize.min.css';
 import { Route } from 'react-router-dom';
 import Home from "./home"
+import Nav from './nav';
 import ChatRoom from "./chat_room"
 
 const App = () => (
     <div>
+        <Nav/>
         <div className="container">
             <Route exact path = "/" component={Home}/>
             <Route path ="/chat-room" component={ChatRoom}/>
